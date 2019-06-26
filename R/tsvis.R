a5f0db3ee305f3b36be4485ccbc463557b3aacbf
Nicholas Berta <45607424+genpack@users.noreply.github.com>
2019-06-26 15:24:35 +1000

    Latest Changes 27 June 2019

--- R/tsvis.R
+++ R/tsvis.R
@@ -6,8 +6,8 @@
 # Author:         Nicolas Berta
 # Email :         nicolas.berta@gmail.com
 # Start Date:     12 October 2018
-# Last Revision:  06 November 2018
-# Version:        0.1.3
+# Last Revision:  27 June 2019
+# Version:        0.1.5
 #
 
 # Version History:
@@ -19,12 +19,14 @@
 # 0.1.1     18 October 2018    Function plot.process.map() modified: does not use plot package any more but prepares customized nodes & links tables
 # 0.1.2     05 November 2018   Function plot.process.map() modified: uses package grviz (renamed from diagramer)
 # 0.1.3     06 November 2018   Function plot.traces.sunburst() modified: builds chartname to avoid re-plotting with same spec
+# 0.1.4     20 June 2019       Function plot.process.map() modified: config argument is merged with default config
+# 0.1.5     27 June 2019       Additional arguments passed to plotting functions.
 
 ############### Process Overview Visualisations:
 
 # plots process map for the transition system
 #' @export plot.process.map
-plot.process.map = function(obj, measure = c('freq', 'time'), time_unit = c('second', 'minute', 'hour', 'day', 'week', 'year'), plotter = c('grviz', 'visNetwork'), config = NULL){
+plot.process.map = function(obj, measure = c('freq', 'time'), time_unit = c('second', 'minute', 'hour', 'day', 'week', 'year'), plotter = c('grviz', 'visNetwork'), config = NULL, ...){
   plotter   = match.arg(plotter)
   measure   = match.arg(measure)
   time_unit = match.arg(time_unit)
@@ -37,10 +39,12 @@ plot.process.map = function(obj, measure = c('freq', 'time'), time_unit = c('sec
 
   nodes = obj$get.nodes()
   links = obj$get.links()
+  
+  if(is.null(config)){config = list()}
 
   cfg = list(link.width.max = 12, link.width.min = 2, link.smooth = list(enabled = T, type = 'curvedCCW'),
              node.label.size = 40, link.label.size = 25, node.physics.enabled = T, layout = 'hierarchical', direction = 'up.down',
-             node.size.min = 2, node.size.max = 4, node.size = 3, node.size.ratio = 0.6)
+             node.size.min = 2, node.size.max = 4, node.size = 3, node.size.ratio = 0.6) %<==>% config
 
   nodes %<>%
     dplyr::mutate(shape = ifelse(status %in% c('START', 'END'), 'circle', ifelse(status %in% c('ENTER', 'EXIT'), 'diamond', 'rectangle')))
@@ -71,7 +75,7 @@ plot.process.map = function(obj, measure = c('freq', 'time'), time_unit = c('sec
       dplyr::mutate(label = ' ' %++% (meanTime %>% round(digits = 2) %>% paste(time_unit %>% substr(1,1))), tooltip = status %>% paste(nextStatus, sep = '-'))
 
     list(nodes = nodes, links = links) %>%
-      viserPlot(key = 'status', shape = 'shape', label = 'label', color = list(color = 'totalDuration'), source = 'status', target = 'nextStatus', linkColor = list(color = 'totalTime'), linkWidth = 'totalTime', linkLabel = 'label', linkTooltip = 'tooltip', config = cfg, plotter = plotter, type = 'graph') -> obj$plots[[plotname]]
+      viserPlot(key = 'status', shape = 'shape', label = 'label', color = list(color = 'totalDuration'), source = 'status', target = 'nextStatus', linkColor = list(color = 'totalTime'), linkWidth = 'totalTime', linkLabel = 'label', linkTooltip = 'tooltip', config = cfg, plotter = plotter, type = 'graph', ...) -> obj$plots[[plotname]]
 
   }
   return(obj$plots[[plotname]])
@@ -79,28 +83,30 @@ plot.process.map = function(obj, measure = c('freq', 'time'), time_unit = c('sec
 
 #' plots process tree for the transition system
 #' @export plot.process.tree
-plot.process.tree = function(obj, plotter = 'sankeytree'){
-  plotter = match.arg(plotter)
+plot.process.tree = function(obj, plotter = 'sankeytree', ...){
+  # plotter = match.arg(plotter)
   tbl = obj$get.traces() %>% buildTreeTable()
   NNN = names(tbl) %-% 'freq'
 
-  tbl %>% sankeytree.tree(label = as.list(NNN[sequence(min(8, length(NNN)))]), size = 'freq')
+  tbl %>% sankeytree.tree(label = as.list(NNN[sequence(min(8, length(NNN)))]), size = 'freq', ...)
 }
 
 #' @export plot.process.sankey
-plot.process.sankey = function(obj){
+plot.process.sankey = function(obj, config = list(), plotter = 'networkD3', ...){
+  cfg = list(node.label.size = 20, node.width = 25) %<==>% config
+  
   list(nodes = obj$get.nodes(), links = obj$get.links()) %>%
-    viserPlot(key = 'status', label = list(label = 'status'), source = list(source = 'status'), target = 'nextStatus', linkWidth = 'totalFreq', type = 'sankey', plotter = 'networkD3', config = list(node.label.size = 20, node.width = 25))
+    viserPlot(key = 'status', label = list(label = 'status'), source = list(source = 'status'), target = 'nextStatus', linkWidth = 'totalFreq', type = 'sankey', config = cfg, ...)
 }
 
 ############### Trace Overview Visualisations:
 
 #' @export plot.traces.bar
-plot.traces.bar  = function(obj, measure = c('freq', 'time'), time_unit = c('second', 'minute', 'hour', 'day', 'week', 'year'), aggregator = c('sum', 'mean', 'median', 'sd'), plotter = 'plotly'){
+plot.traces.bar  = function(obj, measure = c('freq', 'time'), time_unit = c('second', 'minute', 'hour', 'day', 'week', 'year'), aggregator = c('sum', 'mean', 'median', 'sd'), plotter = 'plotly', ...){
   agg = c('sum' = 'tot', 'mean' = 'avg', 'median' = 'med', 'sd' = 'sd')
   ags = c('sum' = 'Total', 'mean' = 'Average', 'median' = 'Median', 'sd' = 'Standard Deviation of')
 
-  plotter    <- match.arg(plotter)
+  # plotter    <- match.arg(plotter)
   measure    <- match.arg(measure)
   time_unit  <- match.arg(time_unit)
   k          <- 1.0/timeUnitCoeff[time_unit]
@@ -115,7 +121,7 @@ plot.traces.bar  = function(obj, measure = c('freq', 'time'), time_unit = c('sec
       dplyr::mutate(freqper = 100*freq/sumfreq) %>%
       dplyr::mutate(tooltip = path %>% paste0('  Freq: ', freq, ' (', freqper %>% round(digits = 1), '%)')) %>%
       dplyr::arrange(freq) %>%
-      viserPlot(x = 'freq', y = 'variation', tooltip = 'tooltip', type = 'bar', plotter = plotter)
+      viserPlot(x = 'freq', y = 'variation', tooltip = 'tooltip', type = 'bar', plotter = plotter, ...)
 
   } else {
     coln = agg[aggregator] %++% 'Time'
@@ -123,12 +129,12 @@ plot.traces.bar  = function(obj, measure = c('freq', 'time'), time_unit = c('sec
     TBL$tooltip = TBL$path %>% paste0('  ', ags[aggregator], ' Process Time: ', TBL %>% pull(agg[aggregator] %++% 'Time') %>% round(digits = 2), ' ',time_unit, 's')
     TBL %>%
       dplyr::arrange_(coln) %>%
-      viserPlot(x = coln, y = 'variation', tooltip = 'tooltip', type = 'bar', plotter = plotter, color = gener::color.mean('red', 'white'))
+      viserPlot(x = coln, y = 'variation', tooltip = 'tooltip', type = 'bar', plotter = plotter, color = gener::color.mean('red', 'white'), ...)
   }
 }
 
 #' @export plot.traces.sunburst
-plot.traces.sunburst = function(obj, min_freq = 10, plotter = c('sunburstr', 'd2b'), width = 600, height = 600){
+plot.traces.sunburst = function(obj, min_freq = 10, plotter = c('sunburstr', 'd2b'), width = 600, height = 600, ...){
   plotter   = match.arg(plotter)
   chartname = paste('traces', 'sunburst', plotter, min_freq, sep = '.')
   if(is.null(obj$plots[[chartname]])){
@@ -136,9 +142,9 @@ plot.traces.sunburst = function(obj, min_freq = 10, plotter = c('sunburstr', 'd2
       dplyr::select(path, freq) %>% dplyr::filter(freq > min_freq - 1)
     if(!is.empty(tbl)){
       if(plotter == 'sunburstr'){
-        tbl %>% sunburstR::sunburst(count = T, width = width, height = height, legend = list(h = 30, w = 120, r = 10, s = 5)) -> obj$plots[[chartname]]
+        tbl %>% sunburstR::sunburst(count = T, width = width, height = height, legend = list(h = 30, w = 120, r = 10, s = 5), ...) -> obj$plots[[chartname]]
       } else if (plotter == 'd2b'){
-        tbl %>% sunburstR::sund2b() -> obj$plots[[chartname]]
+        tbl %>% sunburstR::sund2b(width = width, height = height, ...) -> obj$plots[[chartname]]
       }
     }
   }
@@ -149,7 +155,8 @@ plot.traces.sunburst = function(obj, min_freq = 10, plotter = c('sunburstr', 'd2
 ############### Status Overview Visualisations:
 
 #' @export plot.statuses.bar
-plot.statuses.bar = function(obj, measure = c('freq', 'totTime'), time_unit = c('second', 'minute', 'hour', 'day', 'week', 'year')){
+plot.statuses.bar = function(obj, measure = c('freq', 'totTime'), time_unit = c('second', 'minute', 'hour', 'day', 'week', 'year'), plotter = 'plotly', ...){
+  if(is.empty(obj$history)) return(NULL)
   colname   = c(freq = 'totalEntryFreq', totTime = 'totalDuration')
   vrs       = c(freq = 'Total Entry Freq', totTime = 'Total Duration')
   nontime   = 'freq'
@@ -171,11 +178,11 @@ plot.statuses.bar = function(obj, measure = c('freq', 'totTime'), time_unit = c(
   ND$tooltip <- ND$status %>% paste0('  (', vrs[measure],': ', ND %>% pull(colname[measure]) %>% round(digits = 2), tus, ')')
 
   ND %>% arrange_(colname[measure]) %>%
-    viserPlot(x = colname[measure], y = 'status', tooltip = 'tooltip', plotter = 'plotly', type = 'bar', config = cfg)
+    viserPlot(x = colname[measure], y = 'status', tooltip = 'tooltip', plotter = plotter, type = 'bar', config = cfg, ...)
 }
 
 #' @export plot.statuses.box
-plot.statuses.box = function(obj, time_unit = c('second', 'minute', 'hour', 'day', 'week', 'year')){
+plot.statuses.box = function(obj, time_unit = c('second', 'minute', 'hour', 'day', 'week', 'year'), plotter = 'plotly', ...){
   time_unit = match.arg(time_unit)
   k         = 1.0/timeUnitCoeff[time_unit]
 
@@ -183,13 +190,13 @@ plot.statuses.box = function(obj, time_unit = c('second', 'minute', 'hour', 'day
   H$duration  <- (k*H$duration) %>% round(digits = 2)
 
   cfg = list(title = 'Status Durations', xAxis.label = 'Duration' %>% paste0(' (', substr(time_unit,1,1), ')'), yAxis.label = 'Status')
-  H %>% viserPlot(x = 'duration', y = 'status', plotter = 'plotly', type = 'box', config = cfg)
+  H %>% viserPlot(x = 'duration', y = 'status', plotter = plotter, type = 'box', config = cfg, ...)
 }
 
 ############### Case Overview Visualisations:
 
 #' @export plot.cases.status.pie
-plot.cases.status.pie = function(obj, measure = 'time', aggregator = 'sum', time_unit = c('second', 'minute', 'hour', 'day', 'week', 'year'), trim = NULL){
+plot.cases.status.pie = function(obj, measure = 'time', aggregator = 'sum', time_unit = c('second', 'minute', 'hour', 'day', 'week', 'year'), trim = NULL, plotter = 'plotly', ...){
   measure    = match.arg(measure)
   aggregator = match.arg(aggregator)
   time_unit  = match.arg(time_unit)
@@ -203,12 +210,12 @@ plot.cases.status.pie = function(obj, measure = 'time', aggregator = 'sum', time
   }
   aa$duration  <- aa$duration/timeUnitCoeff[time_unit]
 
-  aa %>% viserPlot(theta = 'duration', label = 'status', config = list(legend.enabled = F), type = 'pie', plotter = 'plotly')
+  aa %>% viserPlot(theta = 'duration', label = 'status', config = list(legend.enabled = F), type = 'pie', plotter = plotter, ...)
 }
 
 #' @export plot.cases.table
-plot.cases.table = function(obj, time_unit = c('second', 'minute', 'hour', 'day', 'week', 'year'), plotter = 'DT', config = list(paging.length = 20)){
-  plotter   = match.arg(plotter)
+plot.cases.table = function(obj, time_unit = c('second', 'minute', 'hour', 'day', 'week', 'year'), plotter = 'DT', config = list(), ...){
+  # plotter   = match.arg(plotter)
   time_unit = match.arg(time_unit)
   k         = 1.0/timeUnitCoeff[time_unit]
 
@@ -219,21 +226,21 @@ plot.cases.table = function(obj, time_unit = c('second', 'minute', 'hour', 'day'
   tbl = tbl[, c('startTime', 'endTime', 'completed', 'duration')]
   cln = list( 'Started at' = 'startTime', 'Ended at' = 'endTime', Completed = 'completed', Duration = 'duration')
   names(cln)[4] %<>% paste0(" (", substr(time_unit,1,1), ")")
-
-  tbl %>% viserPlot(label = cln, plotter = 'DT', type = 'table', config = config, filter = 'top')
+  cfg = list(paging.length = 20) %<==>% config
+  tbl %>% viserPlot(label = cln, plotter = plotter, type = 'table', config = cfg, filter = 'top', ...)
 }
 
 ############### Status Card Visualisations:
 
 #' @export plot.status.gauge
-plot.status.gauge = function(obj, statusID, measure = c('loopRate', 'caseRatio')){
+plot.status.gauge = function(obj, statusID, measure = c('loopRate', 'caseRatio'), ...){
   measure = match.arg(measure)
   # cfg = list(thetaAxis.zone = list( list(min = 0, max = 30, color = 'green'), list(min = 30, max = 60, color = 'yellow'), list(min = 60, max = 100, color = 'red')))
-  c3.gauge(100*obj$get.status.metric(statusID, measure))
+  c3.gauge(100*obj$get.status.metric(statusID, measure), ...)
 }
 
 #' @export plot.status.next.pie
-plot.status.next.pie = function(obj, statusID, trim = NULL, plotter = 'plotly'){
+plot.status.next.pie = function(obj, statusID, trim = NULL, plotter = 'plotly', config = list(), ...){
   if(verify(statusID, 'character', varname = 'statusID') %in% obj$get.statuses()){
     AD = obj$get.adjacency()
     SN = AD[statusID, ] %>% t %>% as.data.frame %>% rownames2Column('status') %>%
@@ -243,11 +250,13 @@ plot.status.next.pie = function(obj, statusID, trim = NULL, plotter = 'plotly'){
   } else {
     SN = data.frame(status = character(), freq = numeric(), stringsAsFactors = F)
   }
-  SN %>% na.omit %>% viserPlot(label = 'status', theta = 'freq', type = 'pie', plotter = plotter, config = list(title = 'Next Status Distribution'))
+  cfg = list(title = 'Next Status Distribution') %<==>% config
+  
+  SN %>% na.omit %>% viserPlot(label = 'status', theta = 'freq', type = 'pie', plotter = plotter, config = cfg, ...)
 }
 
 #' @export plot.status.prev.pie
-plot.status.prev.pie = function(obj, statusID, trim = NULL, plotter = 'plotly'){
+plot.status.prev.pie = function(obj, statusID, trim = NULL, plotter = 'plotly', config = list(), ...){
   if(verify(statusID, 'character', varname = 'statusID') %in% obj$get.statuses()){
     AD = obj$get.adjacency()
     SP = AD[, statusID] %>% as.data.frame
@@ -258,22 +267,23 @@ plot.status.prev.pie = function(obj, statusID, trim = NULL, plotter = 'plotly'){
   } else {
     SP = data.frame(status = character(), freq = numeric(), stringsAsFactors = F)
   }
-
-  SP %>%  na.omit %>% viserPlot(label = 'status', theta = 'freq', type = 'pie', plotter = plotter, config = list(title = 'Previous Status Distribution'))
+  cfg = list(title = 'Previous Status Distribution') %<==>% config
+  SP %>%  na.omit %>% 
+    viserPlot(label = 'status', theta = 'freq', type = 'pie', plotter = plotter, config = cfg, ...)
 }
 
 
 ############### Volume Time Series Visualisations:
 
 #' @export plot.volumes.area
-plot.volumes.area = function(obj, period = 'daily', trim = 0.01, plotter = 'streamgraph'){
+plot.volumes.area = function(obj, period = 'daily', trim = 0.01, plotter = 'streamgraph', ...){
   period = match.arg(period)
   back   = obj$get.backlog(as_timeseries = T, period = period)
   colns  = names(back$data) %-% c('date', 'START', 'END', 'ENTER', 'EXIT')
   sumps  = colSums(back$data[, colns]) %>% vect.normalise
   colns  = names(sumps)[which(sumps >= trim)]
   back$data[, c('date', colns)] %>%
-    viserPlot(x = 'date', y = colns %>% as.list, plotter = plotter, type = 'tsarea')
+    viserPlot(x = 'date', y = colns %>% as.list, plotter = plotter, type = 'tsarea', ...)
 }
 
 
